1130001d83464962437af2c42cae49dec000e1f4
safroalex <alexsafr.a@ya.ru>
2024-03-21 21:30:58 +0300

    translation of quiz

--- korennie_narodi/src/i18n.js
+++ korennie_narodi/src/i18n.js
@@ -61,6 +61,45 @@ const resources = {
         footer_nations_nature: "peoples and nature",
         footer_learn_more: "learn more",
         footer_nations_interview: "peoples and interview",
+
+        //sliderAndQuiz
+        check_your_knowledge: 'check your knowledge',
+        correct_answers_count: 'Number of correct answers',
+        correct_answers_count_from: 'out of',
+        test_completed: 'Test completed',
+
+        fact_correct_answer: 'Yes, this is the correct answer.',
+        fact_wrong_answer: 'No, this is the wrong answer.',
+        fact_indigenous_peoples_count: 'According to the "Unified list of indigenous small-numbered peoples of the Russian Federation", Russia is home to 47 indigenous small-numbered peoples.',
+        fact_kereks_population: 'Kereks. During the 2021 All-Russian population census, only 4 people identified themselves as Kereks. This is the correct answer.',
+
+        quiz_question1: 'Question 1',
+        quiz_question1_text: 'Do you know how many indigenous small-numbered peoples live in Russia?',
+        quiz_question1_option1: 'MORE THAN 50',
+        quiz_question1_option2: '193',
+        quiz_question1_option3: '47',
+        quiz_question1_option4: '29',
+
+        quiz_question2: 'Question 2',
+        quiz_question2_text: 'According to recent studies, which people is now down to just 4 individuals?',
+        quiz_question2_option1: 'KEREKS',
+        quiz_question2_option2: 'YUKAGHIRS',
+        quiz_question2_option3: 'ENETS',
+        quiz_question2_option4: 'OROCHS',
+        
+        quiz_question3: 'Question 3',
+        quiz_question3_text: 'Vargan, komus, khomus, komys, amanhuur, tumran, zubanka - all these names refer to one important item in the cultural life of the northern peoples. Did you guess what it is?',
+        quiz_question3_option1: 'MUSICAL INSTRUMENT',
+        quiz_question3_option2: 'BABY CRADLE',
+        quiz_question3_option3: 'SMOKING PIPE',
+        quiz_question3_option4: 'SPECIAL CEREMONIAL KNIFE',
+        
+        quiz_question4: 'Question 4',
+        quiz_question4_text: 'Representatives of this people live on two continents - Eurasia and North America. The traditional dwelling "valkaran" is a semi-dugout based on a frame of whale bones or fin, covered with dry grass, skins, turf. Known as excellent hunters of whales, seals, sea lions, and walruses. In Russia, they reside in the Kamchatka Krai and on the Commander Islands. Which people are these?',
+        quiz_question4_option1: 'EVENKS',
+        quiz_question4_option2: 'ALEUTS',
+        quiz_question4_option3: 'SAAMI',
+        quiz_question4_option4: 'CHUKCHIS',
     }
   },
   ru: {
@@ -114,6 +153,45 @@ const resources = {
         footer_nations_nature: "народы и природа",
         footer_learn_more: "узнать больше",
         footer_nations_interview: "народы и интервью",
+
+        //sliderAndQuiz
+        check_your_knowledge: 'проверьте свои знания',
+        correct_answers_count: 'Количество правильных ответов',
+        correct_answers_count_from: 'из',
+        test_completed: 'Вы завершили тест',
+
+        fact_correct_answer: 'Да, это правильный ответ',
+        fact_wrong_answer: 'Нет, это неправильный ответ',
+        fact_indigenous_peoples_count: 'Согласно "Единому перечню коренных малочисленных народов Российской Федерации" в России проживает 47 коренных малочисленных народов',
+        fact_kereks_population: 'Кереки. Во время Всероссийской переписи населения в 2021 году в кереки себя записало всего 4 человека. Это правильный ответ.',
+
+        quiz_question1: 'Вопрос 1',
+        quiz_question1_text: 'А вы знаете, сколько коренных малочисленных народов проживает в России?',
+        quiz_question1_option1: 'БОЛЬШЕ 50',
+        quiz_question1_option2: '193',
+        quiz_question1_option3: '47',
+        quiz_question1_option4: '29',
+
+        quiz_question2: 'Вопрос 2',
+        quiz_question2_text: 'В каком народе, согласно последним исследованиям, насчитывается всего 4 человека?',
+        quiz_question2_option1: 'КЕРЕКИ',
+        quiz_question2_option2: 'ЮКАГИРЫ',
+        quiz_question2_option3: 'ЭНЦЫ',
+        quiz_question2_option4: 'ОРОЧИ',
+        
+        quiz_question3: 'Вопрос 3',
+        quiz_question3_text: 'Варган, комус, хомус, комыс, аманхуур, тумран, зубанка - все эти названия обозначают один важный предмет для культурной жизни народов севера. А вы поняли, что это за предмет?',
+        quiz_question3_option1: 'МУЗЫКАЛЬНЫЙ ИНСТРУМЕНТ',
+        quiz_question3_option2: 'ЛЮЛЬКА ДЛЯ РЕБЕНКА',
+        quiz_question3_option3: 'КУРИТЕЛЬНАЯ ТРУБКА',
+        quiz_question3_option4: 'СПЕЦИАЛЬНЫЙ ОБРЯДОВЫЙ НОЖ',
+
+        quiz_question4: 'Вопрос 4',
+        quiz_question4_text: 'Представители этого народа живут на два континента - Евразию и Северную Америку. Традиционное жилище “валкаран” представляет собой полуземлянку на основе каркаса из костей кита или плавника, укрытую сухой травой, шкурами, дерном. Известны как отличные охотники на китов, морских котиков, каланов и сивучей. В России проживают на территории Камчатского края и на Командорских островах. Что это за народ?',
+        quiz_question4_option1: 'ЭВЕНКИ',
+        quiz_question4_option2: 'АЛЕУТЫ',
+        quiz_question4_option3: 'СААМИ',
+        quiz_question4_option4: 'ЧУКЧИ',
     }
   }
 };
